20a7cf6febac4c79bdf1bc1057b5a5f3ed1da730
Drobor <m295-6d@ya.ru>
2016-09-30 23:42:28 +0300

    task07 rewritten

--- task07/blot.pl
+++ task07/blot.pl
@@ -4,14 +4,22 @@ suspect(Who) :- Who=borya;Who=alex;Who=vitya.
 %     '?- boryaStBlot(X), vityaStBlot(X).' is true for X=vitya
 % obviously, i could have just swapped order of this functions in
 % blot(Who) check, but it seems to be worse solution
-alexStBlot(Who) :- Who=borya, not(Who=vitya).
-boryaStBlot(Who) :- Who=vitya, not(Who=alex).
-vityaStBlot(Who) :- not(Who=borya).
-vityaStHW(Who) :- not(Who=vitya).
 
-blot(Who) :- (suspect(Who), alexStBlot(Who), boryaStBlot(Who), not(vityaStBlot(Who))).
-blot(Who) :- (suspect(Who), vityaStBlot(Who), boryaStBlot(Who), not(alexStBlot(Who))).
-blot(Who) :- (suspect(Who), alexStBlot(Who), vityaStBlot(Who), not(boryaStBlot(Who))).
+statement(vitya,blot,first,Who):- not(Who=borya).
+statement(vitya,hw,second,Who):- not(Who=vitya).
+statement(alex,blot,first,Who):- not(Who=vitya).
+statement(alex,blot,second,Who):- Who=borya.
+statement(borya,blot,first,Who):- Who=vitya.
+statement(borya,blot,second,Who):- not(Who=alex).
+
+exist(X,Y,Z):-suspect(Temp),statement(X,Y,Z,Temp).
+skip(X,Y,Z):-not(exist(X,Y,Z)).
+
+chk(Author,Topic,Who) :-(statement(Author,Topic,first,Who);skip(Author,Topic,first)),(statement(Author,Topic,second,Who);skip(Author,Topic,second)).
+
+blot(Who) :- (suspect(Who), chk(alex,blot,Who), chk(borya,blot,Who), not(chk(vitya,blot,Who))).
+blot(Who) :- (suspect(Who), chk(vitya,blot,Who), chk(borya,blot,Who), not(chk(alex,blot,Who))).
+blot(Who) :- (suspect(Who), chk(alex,blot,Who), chk(vitya,blot,Who), not(chk(borya,blot,Who))).
 
 
 

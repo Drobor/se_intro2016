1ce239ded54207043e61a3067fbcccf0e1892ede
Drobor <m295-6d@ya.ru>
2016-10-25 17:06:05 +0300

    13th task completion

---
+++ task13/haskell.hs
@@ -0,0 +1,17 @@
+main = print $ (sqrt_n 65536)
+
+sqrt_n num | num < 0   = (-1) -- undefined throws exception
+           | otherwise = sqrtInternal num (num * 0.5)
+
+sqrtInternal num result | abs (curVal - result) > abs(num * 0.000001) = sqrtInternal num curVal
+                        | otherwise                                   = curVal
+             where curVal = (result + num / result) * 0.5
+
+sum_digits num | num > 0   = (num `mod` 10) + sum_digits (num `div` 10)
+               | num < 0   = sum_digits (-num)
+               | otherwise = 0
+
+count_digits num = digitsCountInternal (abs(num) `div` 10)
+
+digitsCountInternal num | num > 0   = 1 + digitsCountInternal (abs(num) `div` 10)
+                        | otherwise = 1

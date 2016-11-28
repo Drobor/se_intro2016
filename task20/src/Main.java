7f3a2a840934307611488540cf813b1c739e3f5b
Иван Малявкин < >
2016-11-28 23:01:16 +0300

    options fix

--- task20/src/Main.java
+++ task20/src/Main.java
@@ -6,19 +6,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         GameRunner game = new GameRunner();
-        game.setupGame();
-        Scanner sc = new Scanner(System.in);
-        String input = "";
-        while (!input.equals("bench") && !input.equals("play")) {
-            System.out.println("type \"bench\" to test players multiple times or \"play\" to run the game once");
-            input = sc.nextLine();
-        }
-        if (input.equals("bench")) {
-            System.out.println("type test runs count");
-            game.testPlayers(sc.nextInt());
+        if (args.length >= 1 && args[0].trim().toLowerCase().equals("settings")) {
+            game.setupGame();
+            Scanner sc = new Scanner(System.in);
+            String input = "";
+            while (!input.equals("bench") && !input.equals("play")) {
+                System.out.println("type \"bench\" to test players multiple times or \"play\" to run the game once");
+                input = sc.nextLine();
+            }
+            if (input.equals("bench")) {
+                System.out.println("type test runs count");
+                game.testPlayers(sc.nextInt());
+            } else {
+                game.runGame(false);
+            }
         } else {
-            game.runGame(false);
+            game.createDefaultBench();
+            System.out.println("Program has started with default settings (classic ticktacktoe rules ,3x3 field, two random players, 100000 test runs).\nTo specify game settings please run the program with argument \"settings\"\n");
+            game.testPlayers(100000);
         }
     }
-
 }
